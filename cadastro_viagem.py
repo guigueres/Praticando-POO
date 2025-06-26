9510c595de3d3183dde74cd90be69e941a4cad9e
guigueres <macedoguilherme469@gmail.com>
2025-06-26 08:53:01 -0300

    Atualização

--- cadastro_viagem.py
+++ cadastro_viagem.py
@@ -1,7 +1,7 @@
 class Viagem:
     def __init__(self, nome):
         self.nome = nome
-        self.destinos = ["Xique-Xique", "Ilheus", "Ibicarai", "Itapetinga", "Floresta"]
+        self.destinos = ["Xique-Xique-BA", "Ilheus-BA", "Ibicarai-BA", "Itapetinga-BA", "Floresta Azul-BA"]
 
     def escolher_destino(self):
         for i, destino in enumerate(self.destinos, 1):
@@ -10,6 +10,6 @@ class Viagem:
         print(f"{self.nome} escolheu viajar para {self.destinos[escolha - 1]}")
 
 if __name__ == "__main__":
-    nome = input("Nome do viajante: ")
+    nome = input("Nome do viajante: ").title()
     viagem = Viagem(nome)
     viagem.escolher_destino()
